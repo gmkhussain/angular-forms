b92a59e0e98a58a4f2e0a0c74ba7a6a8cfbae2c3
Developer <gmk.hussain@gmail.com>
2021-12-06 15:01:04 +0500

    Binding Data to a Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-froms';
+  topics = ['Angular', 'React', 'Vue'];
+  
+  userModel = new User('Amoos', 'inf@now.me', '48787', '', 'morning' , true ) // <--
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms'; //<--
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
     AppComponent
